a5b693f70aa36da89031022c24cbe3944d1d9f0a
alexdenisova <37692145+alexdenisova@users.noreply.github.com>
2018-10-04 19:05:58 +0700

    Update pascal.c

--- pascal.c
+++ pascal.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int main() {
-  int y, x, fy=1, fx=1, k=1, i=1, n;
-  scanf("%d", &y);
-  scanf("%d", &x);
-  for(i=1; i<=(y-1); i++) {
-    fy=fy*i;
-  }
-  for(i=1; i<=(x-1); i++) {
-    fx=fx*i;
-  }
-  for(i=0; i<=(y-x); i++) {
-    if(i==0){
-      k=1;
-    }else{
-    k=k*i;
-    }
-  }
-  n = fy/(fx*k);
-  printf("%d", n);
-  return 0;
+	int y, x, fy=1, fx=1, k=1, i=1, n;
+	scanf("%d", &y);
+	scanf("%d", &x);
+	for(i=1; i<=(y-1); i++) {
+		fy=fy*i;
+	}
+	for(i=1; i<=(x-1); i++) {
+		fx=fx*i;
+	}
+	for(i=0; i<=(y-x); i++) {
+		if(i==0){
+			k=1;
+		}else{
+			k=k*i;
+		}
+	}
+	n = fy/(fx*k);
+	printf("%d", n);
+	return 0;
 }
